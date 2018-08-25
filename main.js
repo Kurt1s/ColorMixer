d85d9a3eb3e29122f2e2cd04d32302370f4db220
Kurtis Bassmann <knbsk8@gmail.com>
2018-08-25 17:47:59 -0400

    Successfully displayed tertiary colors under respectful div, when a primary and secondary are mixed. This works for 2/3 secondary colors per primary color.

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ function run(){
     var colorsToMix = new Array(0);
 
     function buttonHandler(event){
+        console.log("in handler")
         var color = $(event.target).attr("class")
+        console.log(color);
         colorsToMix.push(color);
         
         if(colorsToMix.length === 2) {
@@ -34,17 +36,36 @@ function run(){
         var c2 = colorsArr[1];
         var c3;
 
+        //handling secondary color name result
         if(c1==="Red" && c2==="Yellow" || c2==="Red" && c1==="Yellow")
             c3 = "Orange";
 
         else if(c1==="Blue" && c2==="Red" || c2==="Blue" && c1==="Red")
-            c3 = "Purple";
+            c3 = "Violet";
         
         else if(c1==="Yellow" && c2==="Blue" || c2==="Yellow" && c1==="Blue")
             c3 = "Green";
         
+        //handling tertiary color name result
+        else if(c1==="Orange" || c2==="Orange"){
+            if(c1==="Red" || c2==="Red")
+                c3 = "Red-Orange"   //
+            else if(c1==="Yellow" || c2==="Yellow")
+                c3 = "Yellow-Orange"//
+        }
+        else if(c1==="Green" || c2==="Green"){
+            if(c1==="Yellow" || c2==="Yellow")
+                c3 = "Yellow-Green" //
+            else if(c1==="Blue" || c2==="Blue")
+                c3 = "Blue-Green"   //
+        }
+        else if(c1==="Violet" || c2==="Violet"){
+            if(c1==="Blue" || c2==="Blue")
+                c3 = "Blue-Violet"  //
+            else if(c1==="Red" || c2==="Red")
+                c3 = "Red-Violet"   //
+        }
         return c3;
-
     }
 
     function addButton(color){
@@ -58,26 +79,45 @@ function run(){
             newButton.text(color)
             newButton.css("background-color", color);
             console.log(newButton)
+            newButton.on("click",buttonHandler);
+
 
             //figure out accurate hex value corresponding to the String passed in.
             var hex;
+                //values for secondary colors
             if(color==="Orange")
-                hex = "#ff8000";
+                hex = "#ff8000"
             else if(color==="Green")
                 hex = "#00ff00"
-            else if(color==="Purple")
+            else if(color==="Violet")
                 hex = "#9900cc"
             
+                //values for tertiary colors
+            else if(color==="Red-Orange")
+                hex = "#ff3300"
+            else if(color==="Yellow-Orange")
+                hex = "#ffbf00"
+            else if(color==="Yellow-Green")
+                hex = "#80ff00"
+            else if(color==="Blue-Green")
+                hex = "#00ff40"
+            else if(color==="Blue-Violet")
+                hex = "#0000ff"
+            else if(color==="Red-Violet")
+                hex = "#bf00ff"
+
 
             //set color of button to hex value
             newButton.css("background-color", hex);
 
             //figure out where to append the button
-            if(color==="Orange" || color==="Green" || color==="Purple") {
+            // if color name is secondary
+            if(color==="Orange" || color==="Green" || color==="Violet") {
                 $("#secondary").append(newButton);
                 
             }
 
+            // if color name is tertiary
             else{
                 $("#tertiary").append(newButton);
             }
